382bf2f44a5d70b651a8076c12fcef415980f3dd
prayatna koirala <koiralaprayatna9@gmail.com>
2018-08-31 15:35:57 +0545

    login prevail between tabs and windows

--- src/App.js
+++ src/App.js
@@ -1,48 +1,50 @@
-import React from 'react';
-import {BrowserRouter as Router,Route
-} from 'react-router-dom';
-import {Provider} from 'react-redux'
-
+import React,{Component} from 'react';
+import { Route,Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 //Components
-import Navbar from './components/layout/Navbar'
-import Homepage from './pages/Homepage'
-import AboutUs from './pages/AboutUs'
-import Contact from './pages/Contact'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Login from './components/auth/LoginContainer'
-import Register from './components/auth/Register'
-import App1 from './components/auth/app'
-import Bookingpage from './pages/Bookingpage'
+import Navbar from './components/layout/Navbar';
+import Homepage from './pages/Homepage';
+import AboutUs from './pages/AboutUs';
+import Contact from './pages/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Login from './components/auth/LoginContainer';
+import Register from './components/auth/Register';
+import App1 from './components/auth/app';
+import Bookingpage from './pages/Bookingpage';
 
 
 //Styles
-import './style/default.scss'
-import store from './redux/store';
-
-class App extends React.Component{
-
-    render(){
-        return(
-            <Provider store={store}>
-            <Router>
-                <div className='App'>
-                    <Header/>
-                    <Navbar/>
-                        <div className="container">
-                            <Route path='/' exact={true} component={Homepage}/>
-                            <Route path='/aboutus' exact={true} component={AboutUs}/>       
-                            <Route path='/login' exact={true} component={Login}/>
-                            <Route path='/register' exact={true} component={Register}/>
-                            <Route path='/contact' exact={true} component={Contact}/>
-                            <Route path='/test' exact={true} component={App1}/>
-                            <Route path='/bookingpage' exact={true} component={Bookingpage}/>
-                        </div>
-                    <Footer/>
-                </div>
-            </Router>
-            </Provider>
-        )   
-    }
+import './style/default.scss';
+import store, { history } from './redux/store';
+import { ConnectedRouter } from 'connected-react-router';
+import LoginChecker from './LoginChecker';
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <div className="App">
+              <Header />
+              <Navbar />
+              <div className="container">
+              <Switch>
+                <Route exact path="/"   render={() =><LoginChecker><Homepage {...this.props}/></LoginChecker>}/>
+                <Route exact path="/aboutus"  render={() =><LoginChecker><AboutUs {...this.props}/></LoginChecker>} />
+                <Route exact path="/login"  component={Login} />
+                <Route exact path="/register"  component={Register}  />
+                <Route exact path="/contact"  render={() =><LoginChecker><Contact {...this.props}/></LoginChecker>} />
+                <Route exact path="/test" render={() =><LoginChecker><App1 {...this.props}/></LoginChecker>} />
+                <Route exact path="/bookingpage"  render={() =><LoginChecker><Bookingpage {...this.props}/></LoginChecker>} />
+                </Switch>
+              </div>
+              <Footer />
+            </div>
+        </ConnectedRouter>
+      </Provider>
+    );
+  }
 }
-export default App
+export default App;
+
+// import * as actionCreator from './';
